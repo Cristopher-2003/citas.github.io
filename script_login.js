182e526b5dc7fa44e4021dd1f96eb99ff77e2d6d
Cristopher-2003 <110250316+Cristopher-2003@users.noreply.github.com>
2023-06-13 13:11:14 -0500

    siuuuuuu

--- script_login.js
+++ script_login.js
@@ -1,25 +1,14 @@
 let password = document.getElementById('password');
 let togglePasword = document.getElementById('toggle');
 
-let password_two = document.getElementById('password_two');
-let toggle_two = document.getElementById('toggle_two');
-
-
-
 function showHide(){
     if(password.type === "password"){
         password.setAttribute('type' , 'text');
         togglePasword.classList.add('hide');
 
-        password_two.setAttribute('type' , 'text');
-        toggle_two.classList.add('hide');
-
     }else{
         password.setAttribute('type' , 'password');
         togglePasword.classList.remove('hide');
-
-        password_two.setAttribute('type' , 'password');
-        toggle_two.classList.remove('hide');
    
     }
 }
